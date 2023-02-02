18993b46cc997ccfaf94965345185e67094ed1a5
Chad ONeal <chadoneal3@gmail.com>
2023-02-02 13:55:01 -0500

    week-5

---
+++ week-5/exercise-5.3-data-tables/in-n-out-booksp2/src/app/book-list/book-list.component.ts
@@ -0,0 +1,41 @@
+/*
+============================================
+, Title: book-list.component.spec.ts
+; Author: Chad ONeal
+; Date: 02/02/2023
+; Description: book-list.component.spec.ts
++===========================================
+*/
+
+// import statements
+import { Component, OnInit } from '@angular/core';
+import { BooksService } from '../books.service';
+import { IBook } from '../book.interface';
+import { Observable } from 'rxjs';
+
+// tells which css and html files to use
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+
+// export statement
+export class BookListComponent implements OnInit {
+
+  books: Observable<IBook[]>;
+  header: Array<string> = ['isbn', 'title', 'numOfPages', 'authors']
+  book: IBook;
+
+  constructor(private booksService: BooksService) {
+    this.books = this.booksService.getBooks();
+  }
+
+  ngOnInit(): void {
+  }
+
+  showBookDetails(isbn: string) {
+    this.book = this.booksService.getBook(isbn);
+    console.log(this.book);
+  }
+}
