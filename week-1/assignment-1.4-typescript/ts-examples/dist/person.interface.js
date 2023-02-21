df2048323424265a078e7acd37d39a38deab404f
Chad ONeal <chadoneal3@gmail.com>
2023-02-21 17:35:38 -0500

    adding files

---
+++ week-1/assignment-1.4-typescript/ts-examples/dist/person.interface.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * Title: person.interface.ts
+ * Author: Chad ONeal
+ * Date: 01/03/2023
+ * Description: Person interface
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
