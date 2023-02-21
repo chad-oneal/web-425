38a4418e7ba0541a0f23cedab1d639ad92226194
Chad ONeal <chadoneal3@gmail.com>
2023-02-21 17:39:10 -0500

    Delete week-1/assignment-1.4-typescript/ts-examples directory

--- week-1/assignment-1.4-typescript/ts-examples/dist/person.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Title: person.interface.ts
- * Author: Chad ONeal
- * Date: 01/03/2023
- * Description: Person interface
- */
-Object.defineProperty(exports, "__esModule", { value: true });
